0500595a37a17e14e1b5c52aa4462d86fcdfd1ba
James Prince <james.prince1@gmail.com>
2022-11-08 14:22:26 +1100

    Added relationship definitions

--- models/index.js
+++ models/index.js
@@ -4,13 +4,34 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+
+//A Category has many products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
-// Categories have many Products
+//Connect to transaction tables
+Tag.hasMany(ProductTag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
-// Tags belongToMany Products (through ProductTag)
+//Define relationship
+Tag.belongsToMany(Product, { through: ProductTag});
+Product.belongsToMany(Tag, { through: ProductTag});
+
 
 module.exports = {
   Product,
