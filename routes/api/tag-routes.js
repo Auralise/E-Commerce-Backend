ae002e87665c50531c80f0b018717e9b3822b15f
James Prince <james.prince1@gmail.com>
2022-11-10 13:53:07 +1100

    Added get specific tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,9 +20,26 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+
+    if (tag){
+      res.status(200).json(tag);
+    } else {
+      res.status(404).json({
+        message: `No tag with ID ${req.params.id} found`,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "An internal server error occured",
+    });
+  }
 });
 
 router.post('/', (req, res) => {
